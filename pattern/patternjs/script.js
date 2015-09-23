8765115912846da0896bed00535ba21fddcf02ee
Anna Courchaine <askccourchaine@gmail.com>
2015-09-23 01:29:54 -0500

    pattern
    Pattern assignment, jquery practice

---
+++ pattern/patternjs/script.js
@@ -0,0 +1,197 @@
+$(document).ready(function() {
+	
+	slide ();
+	
+	function slide() {
+	$("#circlea1").animate({left: "100px"}, 2000);
+	$("#circlea1").animate({left: "50px"}, 2000, slide);
+	}
+	
+	
+	slide2 ();
+	function slide2() {
+	$("#circlea3").animate({left: "230px"}, 2000);
+	$("#circlea3").animate({left: "300px"}, 2000, slide2);
+	}
+	
+	
+	slide3 ();
+	function slide3() {
+	$("#circlea7").animate({top: "120px"}, 2000);
+	$("#circlea7").animate({top: "220px"}, 2000, slide3);
+	}
+	
+	slide4 ();
+	function slide4() {
+	$("#circlea5").animate({top: "120px"}, 2000);
+	$("#circlea5").animate({top: "10px"}, 2000, slide4);
+	}
+	
+	
+	slide5 ();
+	function slide5() {
+	$("#circlea9").animate({left: "500px"}, 2000);
+	$("#circlea9").animate({left: "430px"}, 2000, slide5);
+	}
+	
+	slide6 ();
+	function slide6() {
+	$("#circlea23").animate({left: "600px"}, 2000);
+	$("#circlea23").animate({left: "700px"}, 2000, slide6);
+	}
+	
+	slide7 ();
+	function slide7() {
+	$("#circlea11").animate({left: "100px"}, 2000);
+	$("#circlea11").animate({left: "50px"}, 2000, slide7);
+	}
+	
+	slide8 ();
+	function slide8() {
+	$("#circlea13").animate({left: "200px"}, 2000);
+	$("#circlea13").animate({left: "300px"}, 2000, slide8);
+	}
+	
+	slide9 ();
+	function slide9() {
+	$("#circlea17").animate({top: "550px"}, 2000);
+	$("#circlea17").animate({top: "660px"}, 2000, slide9);
+	}
+	
+	slide10 ();
+	function slide10() {
+	$("#circlea15").animate({top: "550px"}, 2000);
+	$("#circlea15").animate({top: "440px"}, 2000, slide10);
+	}
+	
+	slide11 ();
+	function slide11() {
+	$("#circlea19").animate({left: "520px"}, 2000);
+	$("#circlea19").animate({left: "430px"}, 2000, slide11);
+	}
+	
+	slide12 ();
+	function slide12() {
+	$("#circlea27").animate({left: "600px"}, 2000);
+	$("#circlea27").animate({left: "700px"}, 2000, slide12);
+	}
+	
+	slide13 ();
+	function slide13() {
+	$("#circlea21").animate({left: "-310px"}, 2000);
+	$("#circlea21").animate({left: "-110px"}, 2000, slide13);
+	}
+	
+	slide14 ();
+	function slide14() {
+	$("#circlea25").animate({left: "-310px"}, 2000);
+	$("#circlea25").animate({left: "-110px"}, 2000, slide14);
+	}
+	
+	slide15 ();
+	function slide15() {
+	$("#circlea4").animate({top: "120px"}, 2000);
+	$("#circlea4").animate({top: "10px"}, 2000, slide15);
+	}
+	
+	slide16 ();
+	function slide16() {
+	$("#circlea10").animate({left: "240px", top: "140px"}, 2000);
+	$("#circlea10").animate({left: "100px", top: "230px"}, 2000, slide16);
+	}
+	
+	slide17 ();
+	function slide17() {
+	$("#circlea12").animate({left: "270px", top: "140px"}, 2000);
+	$("#circlea12").animate({left: "370px", top: "230px"}, 2000, slide17);
+	}
+	
+	slide18 ();
+	function slide18() {
+	$("#circlea14").animate({left: "640px", top: "260px"}, 2000);
+	$("#circlea14").animate({left: "640px", top: "230px"}, 2000, slide18);
+	}
+	
+	slide19 ();
+	function slide19() {
+	$("#circlea20").animate({left: "620px", top: "280px"}, 2000);
+	$("#circlea20").animate({left: "500px", top: "440px"}, 2000, slide19);
+	}
+	
+	slide20 ();
+	function slide20() {
+	$("#circlea22").animate({left: "650px", top: "280px"}, 2000);
+	$("#circlea22").animate({left: "750px", top: "440px"}, 2000, slide20);
+	}
+	
+	
+	slide21 ();
+	function slide21() {
+	$("#circlea18").animate({left: "220px", top: "520px"}, 2000);
+	$("#circlea18").animate({left: "250px", top: "440px"}, 2000, slide21);
+	}
+	
+	slide22 ();
+	function slide22() {
+	$("#circlea24").animate({left: "200px", top: "540px"}, 2000);
+	$("#circlea24").animate({left: "90px", top: "660px"}, 2000, slide22);
+	}
+	
+	slide23 ();
+	function slide23() {
+	$("#circlea26").animate({left: "240px", top: "540px"}, 2000);
+	$("#circlea26").animate({left: "360px", top: "660px"}, 2000, slide23);
+	}
+	
+	slide24 ();
+	function slide24() {
+	$("#circlea29").animate({left: "630px", top: "700px"}, 2000);
+	$("#circlea29").animate({left: "520px", top: "800px"}, 2000, slide24);
+	}
+	
+	slide25 ();
+	function slide25() {
+	$("#circlea30").animate({left: "660px", top: "700px"}, 2000);
+	$("#circlea30").animate({left: "700px", top: "800px"}, 2000, slide25);
+	}
+	
+	slide26 ();
+	function slide26() {
+	$("#circlea28").animate({left: "640px", top: "680px"}, 2000);
+	$("#circlea28").animate({left: "640px", top: "660px"}, 2000, slide26);
+	}
+	
+	slide27 ();
+	function slide27() {
+	$("#circlea2").animate({left: "-100px", top: "-100px"}, 2000);
+	$("#circlea2").animate({left: "0px", top: "10px"}, 2000, slide27);
+	}
+	
+	slide28 ();
+	function slide28() {
+	$("#circlea6").animate({left: "540px", top: "-100px"}, 2000);
+	$("#circlea6").animate({left: "500px", top: "10px"}, 2000, slide28);
+	}
+	
+	slide29 ();
+	function slide29() {
+	$("#circlea8").animate({left: "720px", top: "-100px"}, 2000);
+	$("#circlea8").animate({left: "740px", top: "10px"}, 2000, slide29);
+	}
+	
+	slide30 ();
+	function slide30() {
+	$("#circlea16").animate({left: "-100px", top: "440px"}, 2000);
+	$("#circlea16").animate({left: "-20px", top: "440px"}, 2000, slide30);
+	}
+	
+	
+	
+
+	
+	
+		
+		
+		
+		
+});
